ff80147caf4eccc43e48d4f0b5a5f553927fe9cf
hhw1101 <jenniferhuang2002@gmail.com>
2022-03-05 21:50:18 +0000

    graphics folder added

--- sketch.js
+++ sketch.js
@@ -1,14 +1,16 @@
 let cardBack;
-function preload(){
-  cardBack = loadImage("Graphics/card.jpg");
-  
+function preload() {
+  cardBack = loadImage("Graphics/cardbacking.png");
+
 }
 function setup() {
   createCanvas(400, 400);
+
+  Image(cardBack, 0, 0);
 }
 
 
 function draw() {
   background(220);
-  circle(100,100,100);
+  circle(100, 100, 100);
 }
