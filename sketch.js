563f4fcba4277309538276a2e84090fa3bb2784b
hhw1101 <jenniferhuang2002@gmail.com>
2022-03-06 10:42:53 +0000

    tidying up and starting logic

--- sketch.js
+++ sketch.js
@@ -2,10 +2,13 @@
 let cardBack;
 let bg;
 let cards = [];
-
+let imageName = ["apple.webp", 'banana.webp', 'cat.png', 'dog.png', 'orange.jpg', 'pear.png']
+let images = []
 function preload() {
-  cardBack = loadImage("Graphics/cardbacking.png");
+  cardBack = loadImage("Graphics/Card/cardbacking.png");
   bg = loadImage("Graphics/bg.jpg");
+  for (let i = 0; i < imageName.length; i++)
+    append(images, loadImage('Graphics/Objects/' + i))
 }
 
 function setup() {
@@ -15,8 +18,9 @@ function setup() {
 
   let cardWidth = windowWidth / 5
   let cardHeight = floor(windowHeight / 2.5)
-  for (let i = 0; i < 5; i++) {
-    cards[i] = new Card(i * cardWidth + 10, cardHeight / 2, cardWidth, cardHeight);
+
+  for (let i = 0; i < 4; i++) {
+    cards[i] = new Card(i * cardWidth + 100, cardHeight / 2, cardWidth, cardHeight, random(images));
 
   }
 }
