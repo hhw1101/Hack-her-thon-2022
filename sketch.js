06213e02da98c83b4be57d9d31b6b1ce30007c3e
hhw1101 <jenniferhuang2002@gmail.com>
2022-03-06 04:08:06 +0000

    displaying cards and background

--- sketch.js
+++ sketch.js
@@ -1,15 +1,18 @@
 
 let cardBack;
+let bg;
 let cards = [];
 
 function preload() {
   cardBack = loadImage("Graphics/cardbacking.png");
-
+  bg = loadImage("Graphics/bg.jpg");
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  background(bg)
   // image(cardBack, 0, 0, 13,5);
+
   let cardWidth = windowWidth / 5
   let cardHeight = floor(windowHeight / 2.5)
   for (let i = 0; i < 5; i++) {
@@ -23,7 +26,7 @@ function windowResized() {
 }
 
 function draw() {
-  background(220);
+  //background(220);
   for (let i = 0; i < 5; i++) {
     cards[i].show();
 
